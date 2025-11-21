2b37eceac62c072104e42eebf1e236d5fa75dbd6
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2025-11-21 13:08:56 +0000

    refactor: move features array to new file

---
+++ src/data/features.ts
@@ -0,0 +1,25 @@
+export const features = [
+  {
+    title: 'Clientes',
+    description: 'Gere os teus clientes de todos os ginásios onde trabalhas.',
+  },
+  {
+    title: 'Planos de treino',
+    description:
+      'Cria planos de treino orientados para os objetivos específicos de cada cliente.',
+  },
+  {
+    title: 'Avaliações físicas',
+    description:
+      'Acompanha o progresso dos teus clientes com avaliações físicas regulares.',
+  },
+  {
+    title: 'Calendário',
+    description:
+      'Insere no teu calendário os comprimissos e sessões de treino com os teus clientes.',
+  },
+  {
+    title: 'Faturação',
+    description: 'Controla os pagamentos dos teus clientes',
+  },
+]
