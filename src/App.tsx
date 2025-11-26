219b39e69e65f0221645a7c453342a7498b3dc91
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2025-11-26 08:25:18 +0000

    feature: use react router dom to navigate between tos, privacy and landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
+import { Route, Routes } from 'react-router-dom'
+
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Features from '@/components/Features'
 import Pricing from '@/components/Pricing'
 import Footer from '@/components/Footer'
 
+import Privacy from '@/pages/Privacy'
+import ToS from '@/pages/ToS'
+
 function App() {
   return (
-    <div>
-      <Header />
-      <Hero />
-      <Features />
-      <Pricing />
-      <Footer />
-    </div>
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <div>
+            <Header />
+            <Hero />
+            <Features />
+            <Pricing />
+            <Footer />
+          </div>
+        }
+      />
+      <Route path='/tos' element={<ToS />} />
+      <Route path='/privacy' element={<Privacy />} />
+    </Routes>
   )
 }
 
