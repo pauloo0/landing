9fb728346658962decc34d837a32557a14539a41
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2025-11-27 07:51:38 +0000

    feature: add live priceIds

--- src/data/pricing.ts
+++ src/data/pricing.ts
@@ -5,7 +5,7 @@ export const pricingPlans = [
     promo_price: '19,99€',
     price: '24,99€',
     interval: 'month',
-    priceId: 'price_1STSnYHeL2ihwztL7VASQ7dW',
+    priceId: 'price_1SXpCzHohrCxNepJWQXgXRnU',
     features: [
       'Gestão ilimitada de clientes',
       'Criação de treinos personalizados',
@@ -20,7 +20,7 @@ export const pricingPlans = [
     promo_price: '199,99€',
     price: '249,99€',
     interval: 'year',
-    priceId: 'price_1STSnYHeL2ihwztLQioVoOd3',
+    priceId: 'price_1SXpECHohrCxNepJ9BFhxgAy',
     popular: true,
     savings: 'Poupe 17%',
     features: [
